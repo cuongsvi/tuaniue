918ae65f995fd0cb318f7697509379e835d836f7
cuongsvi <162500153+cuongsvi@users.noreply.github.com>
2024-03-08 12:20:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const nameGirl = 'Em';
+const nameGirl = 'Em Hiền';
 const giftUrl = 'https://www.facebook.com/vnt.1804';
 const eventName = 'Chúc Mừng 8/3';
-const titleCard = 'Tặng người ấy';
+const titleCard = 'Tặng Thanh Hiền';
 const contentCard = 'Chúc honey của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 const giftImage = 'anh.png';
